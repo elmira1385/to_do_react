16818711cb572033a9c14c1c9905102c0879436e
elmira1385 <elmirashokriyan64@gmail.com>
2025-11-02 15:21:49 +0330

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { usetodo } from './store/useToDo';
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, settitle] = useState("")
+  const{todo,setTodo} =usetodo()
 
   return (
     <div className="flex flex-col h-full justify-center items-center">
@@ -11,16 +15,29 @@ function App() {
         <p className="text-[#063251] text-[28px] font-semibold flex self-center">
           Grocery Bud
         </p>
-        <form className="flex ">
-          <input
+        <form onSubmit={(e)=>{
+          e.preventDefault();
+          setTodo({id:uuidv4(),title:title})
+          settitle("")
+          }} className="flex ">
+          <input value={title} onChange={(e)=>{
+             settitle(e.target.value)
+          }}
             className="flex-1 p-1 rounded-l-md"
             placeholder="e.g. bread"
             type="text"
           />
-          <button className="px-2 py-1 bg-[#a5d5f8] text-[18px] rounded-r-md">
+          <button  type='submit' className="px-2 py-1 bg-[#a5d5f8] text-[18px] rounded-r-md hover:text-white hover:bg-blue-400">
             submit
           </button>
         </form>
+        <ul className='flex flex-col gap-2'>
+          {
+            todo.map((items)=>(
+              <li className='hover:text-[#063251] hover:bg-blue-100 px-2 py-1 rounded-md'>{items.title}</li>
+            ))
+          }
+        </ul>
       </div>
     </div>
   );
--- src/store/useToDo.ts
+++ src/store/useToDo.ts
@@ -1,1 +1,18 @@
+import { create } from 'zustand'
+
+type Todo = {
+  todo:{
+  id:string
+  title:string
+  }[],
+  setTodo: (todo:{
+  id:string
+  title:string
+  }) => void
+}
+
+export const usetodo = create<Todo>()((set) => ({
+  todo:[],
+  setTodo: (newTodo) => set((todos) => ({todo:[...todos.todo,newTodo]})),
+}))
 
