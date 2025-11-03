bbc88f299063adfe1548b7ee4497bb1d369a25ee
elmira1385 <elmirashokriyan64@gmail.com>
2025-11-03 19:18:44 +0330

    v5

--- src/store/useToDo.ts
+++ src/store/useToDo.ts
@@ -1,26 +1,34 @@
 
-import { create } from 'zustand'
 
-type Todo = {
-  todo:{
-  id:string
-  title:string
-  }[],
-  setTodo: (todo:{
-  id:string
-  title:string
-  }) => void,
-  clearAll:()=>void,
-  clearone:(id:string)=>void,
-  edit:(id:string,newtitle:string)=>void
 
-}
 
-export const usetodo = create<Todo>()((set) => ({
-  todo:[],
-  setTodo: (newTodo) => set((todos) => ({todo:[...todos.todo,newTodo]})),
-  clearAll:()=>set({todo:[]}),
-  clearone:(id)=>set((previous)=>({todo:previous.todo.filter((allitem)=>allitem.id !==id)})),
-  edit:(id,newtitle)=>set((previous)=>({todo:previous.todo.map((item)=>item.id===id ?{...item,title:newtitle}:item)}))
-}))
+
+
+
+// import { create } from 'zustand'
+
+// type Todo = {
+//   todo:{
+//   id:string
+//   title:string
+//   }[],
+//   setTodo: (todo:{
+//   id:string
+//   title:string
+//   }) => void,
+//   clearAll:()=>void,
+//   clearone:(id:string)=>void,
+//   edit:(id:string,newtitle:string)=>void
+
+// }
+
+// export const usetodo = create<Todo>()((set) => ({
+//   todo:[],
+//   setTodo: (newTodo) => set((todos) => ({todo:[...todos.todo,newTodo]})),
+//   clearAll:()=>set({todo:[]}),
+//   clearone:(id)=>set((previous)=>({todo:previous.todo.filter((allitem)=>allitem.id !==id)})),
+//   edit:(id,newtitle)=>set((previous)=>({todo:previous.todo.map((item)=>item.id===id ?{...item,title:newtitle}:item)}))
+// }))
+
+
 
